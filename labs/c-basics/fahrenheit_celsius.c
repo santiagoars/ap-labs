1cdf6f8db1dd1ea479739619758d9714453ae0c5
“Santiago <santiagoas77@gmail.com>
2020-08-25 13:07:04 -0500

    Solving the farenheit to celsius lab

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,3 +1,4 @@
+#include<stdlib.h>
 #include <stdio.h>
 
 #define   LOWER  0       /* lower limit of table */
@@ -6,12 +7,24 @@
 
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int argc, char ** argv)
 {
     int fahr;
-
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
+    if(argc == 2){
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", atoi(argv[1]), (5.0/9.0) * (atoi(argv[1])-32));
+    }else if(argc == 3){
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", atoi(argv[1]), (5.0/9.0) * (atoi(argv[1])-32));
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", atoi(argv[2]), (5.0/9.0) * (atoi(argv[2])-32));
+    }else if(argc == 4) {
+        if(atoi(argv[3]) <= 0){
+            printf("Invalid parameters!!\n");
+        }else{
+            for (fahr = atoi(argv[1]); fahr <= atoi(argv[2]); fahr = fahr + atoi(argv[3]))
+            printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+        }
+    }else{
+        for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+	    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
     return 0;
 }
