7615d280d020ce1a636bce0aaacfa152d1922d97
“Santiago <santiagoas77@gmail.com>
2020-09-01 16:23:28 -0500

    Second lab

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
     // Place your magic here
+    int i = 0;
+    char x;
+    char str[256] = "";
+    while((x = getchar()) != EOF){
+        if(x == '\n'){
+            for(int j = strlen(str); j >= 0; j--){
+                printf(" %c ",str[j]);
+            }
+            printf("\n");
+            memset(str, 0, sizeof str);
+            i = 0;
+            continue;
+        }
+        str[i++] = x;
+    }
+
     return 0;
 }
