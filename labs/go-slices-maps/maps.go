c92069f569b65e2f749c051f1d96f99224f90e03
“Santiago <santiagoas77@gmail.com>
2020-09-22 17:39:53 -0500

    Go slices and maps

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
 func main() {
+	wc.Test(WordCount)
+}
+
+func WordCount(s string) map[string]int {
+	palabras := strings.Split(s, " ")
+	mapa := make(map[string]int)
+	for i := 0; i < len(palabras); i++ {
+
+		mapa[palabras[i]] += 1
+	}
+	return mapa
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,20 @@
 package main
 
+import "golang.org/x/tour/pic"
+
 func main() {
+	pic.Show(Picture)
+}
+
+func Picture(x, y int) [][]uint8 {
+
+	myPic := make([][]uint8, y)
+
+	for i := 0; i < y; i++ {
+		myPic[i] = make([]uint8, x)
+		for j := 0; j < x; j++ {
+			myPic[i][j] = uint8(i ^ j)
+		}
+	}
+	return myPic
 }
